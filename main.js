16cf38469f2fc88ea92b3ed49a3d515b5a2b3262
Daniel Lugo <58088181+dalugoSU@users.noreply.github.com>
2021-02-18 02:02:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function alertMessage() {
+
+    var user_name = prompt("What's your name?")
+
+    if (user_name == null) {
+        user_name = "Admission's Officer"
+    }
+
+    alert("I hope I can be a part of this fall 2021's class for master's in computer science!!")
+    alert(`Thank you for visiting my page ${user_name}!!`)
+
+}
+
